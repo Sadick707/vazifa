bc16dcab16674f3f528440453199d6a0ba7243cb
Sadick707 <muxammadsodiqfaxridinov@gmail.com>
2023-03-13 14:45:02 +0500

    massage

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Provider } from './context/context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
